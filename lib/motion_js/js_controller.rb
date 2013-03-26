d8156bfb2447204a3a798c699c714c93e6c38f21
Michael Erasmus <michaelerasmus@gmail.com>
2013-03-26 19:09:14 +0200

    Really running javascript

--- lib/motion_js/js_controller.rb
+++ lib/motion_js/js_controller.rb
@@ -1,5 +1,11 @@
 class JSController < UIViewController
   def execute(js_string)
-    "2"
+    web_view.stringByEvaluatingJavaScriptFromString(js_string)
   end
+
+  private
+  def web_view
+    @web_view ||= UIWebView.alloc.init.tap{|w| w.delegate = self}
+  end
+
 end
--- spec/js_controller_spec.rb
+++ spec/js_controller_spec.rb
@@ -6,4 +6,10 @@ describe "JSController" do
   it "can execute JS" do
     @controller.execute("1 + 1").should == "2"
   end
+
+  it "can define functions and then execute them" do
+    @controller.execute("function foo(){return 'bar';}")
+    @controller.execute("foo()").should == 'bar'
+  end
+
 end

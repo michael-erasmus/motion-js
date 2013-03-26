64b67567b9ff84487d2a21d35604476443bffd0f
Michael Erasmus <michaelerasmus@gmail.com>
2013-03-26 20:41:28 +0200

    Break up spec

--- spec/js_controller_spec.rb
+++ spec/js_controller_spec.rb
@@ -3,26 +3,31 @@ describe "JSController" do
     @controller = JSController.alloc.init
   end
 
-  it "can execute JS" do
-    @controller.execute("1 + 1").should == "2"
-  end
-
-  it "can define functions and then execute them" do
-    @controller.execute("function foo(){return 'bar';}")
-    @controller.execute("foo()").should == 'bar'
-  end
-
-  it "loads index.html file from the main bundle by default, and calls back when done" do
-    loaded = false
-    @controller.load_html_file do
-      loaded = true
-      @controller.execute("definedInIndex()").should == "present!"
+  describe "#execute" do
+    it "can execute JS" do
+      @controller.execute("1 + 1").should == "2"
     end
-    RunLoopHelpers.proper_wait 1
-    loaded.should == true
+
+    it "can define functions and then execute them" do
+      @controller.execute("function foo(){return 'bar';}")
+      @controller.execute("foo()").should == 'bar'
+    end
   end
 
-  it "complains when trying to load a file that's not there" do
-    -> { @controller.load_html_file("nothere")}.should.raise IOError
+  describe "#load_html_file" do
+    it "loads index.html file from the main bundle by default, and calls back when done" do
+      loaded = false
+      @controller.load_html_file do
+        loaded = true
+        @controller.execute("definedInIndex()").should == "present!"
+      end
+      RunLoopHelpers.proper_wait 1
+      loaded.should == true
+    end
+
+    it "complains when trying to load a file that's not there" do
+      -> { @controller.load_html_file("nothere")}.should.raise IOError
+    end
   end
+
 end
